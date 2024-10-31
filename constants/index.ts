f9a40b6f2f98f87eea02ec405fd8fb657607c444
Justoo1 <amankrahjay@gmail.com>
2024-10-31 18:11:49 +0000

    Implement RIght sidebar

--- constants/index.ts
+++ constants/index.ts
@@ -35,3 +35,19 @@ export const sidebarLinks = [
     label: "Ask a question",
   },
 ];
+
+export const hotQuestions = [
+  { _id: "1", title: "How to create a custom hook in React?" },
+  { _id: "2", title: "How to use React Query?" },
+  { _id: "3", title: "How to use Redux?" },
+  { _id: "4", title: "How to use React Router?" },
+  { _id: "5", title: "How to use React Context?" },
+];
+
+export const popularTags = [
+  { _id: "1", name: "react", questions: 100 },
+  { _id: "2", name: "javascript", questions: 200 },
+  { _id: "3", name: "typescript", questions: 150 },
+  { _id: "4", name: "nextjs", questions: 50 },
+  { _id: "5", name: "react-query", questions: 75 },
+];
